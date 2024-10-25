fedc1a10a60227fd835405402871060f051c94ef
Szabolcs Mladoniczky <szabimlado@gmail.com>
2024-10-25 13:32:30 +0100

    automatically changed text colour to contrast better with the node's fill colour.

--- frontend/graphTypes/FDT.js
+++ frontend/graphTypes/FDT.js
@@ -1,3 +1,48 @@
+function padZero(str, len) {
+    len = len || 2;
+    var zeros = new Array(len).join('0');
+    return (zeros + str).slice(-len);
+}
+
+function invertColor(hex, bw) {
+    if (hex.indexOf('#') === 0) {
+        hex = hex.slice(1);
+    }
+    // convert 3-digit hex to 6-digits.
+    if (hex.length === 3) {
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length !== 6) {
+        throw new Error('Invalid HEX color.');
+    }
+    var r = parseInt(hex.slice(0, 2), 16),
+        g = parseInt(hex.slice(2, 4), 16),
+        b = parseInt(hex.slice(4, 6), 16);
+    if (bw) {
+        // https://stackoverflow.com/a/3943023/112731
+        return (r * 0.299 + g * 0.587 + b * 0.114) > 186
+            ? '#000000'
+            : '#FFFFFF';
+    }
+    // invert color components
+    r = (255 - r).toString(16);
+    g = (255 - g).toString(16);
+    b = (255 - b).toString(16);
+    // pad each with zeros and return
+    return "#" + padZero(r) + padZero(g) + padZero(b);
+}
+
+function hslToHex(h, s, l) {
+    l /= 100;
+    const a = s * Math.min(l, 1 - l) / 100;
+    const f = n => {
+        const k = (n + h / 30) % 12;
+        const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+        return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+    };
+    return `#${f(0)}${f(8)}${f(4)}`;
+}
+
 function sfc32(a, b, c, d) {
     return function () {
         a |= 0; b |= 0; c |= 0; d |= 0;
@@ -11,7 +56,7 @@ function sfc32(a, b, c, d) {
     }
 }
 
-const seedgen = () => (0) >>> 0;
+const seedgen = () => (3) >>> 0;
 var getRand = sfc32(seedgen(), seedgen(), seedgen(), seedgen());
 
 drag = simulation => {
@@ -89,7 +134,7 @@ function generateColor() {
     const sat = Math.floor(getRand() * 50 + 50)
     const val = Math.floor(getRand() * 40 + 40)
 
-    return `hsl(${hue}, ${sat}%, ${val}%)`
+    return hslToHex(hue, sat, val)
 }
 
 function createFDTGraph(rootNode, viewbox) {
@@ -249,6 +294,7 @@ function createFDTGraph(rootNode, viewbox) {
             .attr("alignment-baseline", "central")
             .attr("dominant-baseline", "central")
             .attr("text-anchor", "middle")
+            .attr("fill", d => invertColor(d.color, true))
             .call(wrap, 10);
 
         node = nodeEnter.merge(node)
